40d639479b7e1ea2658b70d8a5165cd72c6cae51
Mamohapi <felicitynchocho@gmail.com>
2024-10-13 10:52:59 +0200

    Updated css

--- javascript/hello.js
+++ javascript/hello.js
@@ -1,34 +1,49 @@
 function updateTime() {
-// Los Angeles
-let losAngelesElement = document.querySelector("#los-angeles");
-if (losAngelesElement) {
-let losAngelesDateElement = losAngelesElement.querySelector(".date");
-let losAngelesTimeElement = losAngelesElement.querySelector(".time");
-let losAngelesTime = moment().tz("America/Los_Angeles");
+// Phuthaditjhaba
+let phuthaditjhabaElement = document.querySelector("#phuthaditjhaba");
+if (phuthaditjhabaElement) {
+let phuthaditjhabaDateElement = phuthaditjhabaElement.querySelector(".date");
+let phuthaditjhabaTimeElement = phuthaditjhabaElement.querySelector(".time");
+let phuthaditjhabaTime = moment().tz("Africa/Phuthaditjhaba");
 
 
-losAngelesDateElement.innerHTML = losAngelesTime.format("MMMM Do YYYY");
-losAngelesTimeElement.innerHTML = losAngelesTime.format(
+phuthaditjhabaDateElement.innerHTML = phuthaditjhabaTime.format("MMMM Do YYYY");
+phuthaditjhabaTimeElement.innerHTML = phuthaditjhabaTime.format(
 "h:mm:ss [<small>]A[</small>]"
 );
- }
+}
 
 
-// Paris
-let parisElement = document.querySelector("#paris");
-if (parisElement) {
-let parisDateElement = parisElement.querySelector(".date");
-let parisTimeElement = parisElement.querySelector(".time");
-let parisTime = moment().tz("Europe/Paris");
+// Cairo
+let cairoElement = document.querySelector("#cairo");
+if (cairoElement) {
+let cairoDateElement = cairoElement.querySelector(".date");
+let cairoTimeElement = cairoElement.querySelector(".time");
+let cairoTime = moment().tz("Africa/Cairo");
 
 
-parisDateElement.innerHTML = parisTime.format("MMMM Do YYYY");
-parisTimeElement.innerHTML = parisTime.format(
+cairoDateElement.innerHTML = cairoTime.format("MMMM Do YYYY");
+cairoTimeElement.innerHTML = cairoTime.format(
 "h:mm:ss [<small>]A[</small>]"
 );
+}
+
+// Lagos
+let lagosElement = document.querySelector("#lagos");
+if (lagosElement) {
+let lagosDateElement = lagosElement.querySelector(".date");
+let lagosTimeElement = lagosElement.querySelector(".time");
+let lagosTime = moment().tz("Africa/Lagos");
+
+
+lagosDateElement.innerHTML = lagosTime.format("MMMM Do YYYY");
+lagosTimeElement.innerHTML = lagosTime.format(
+"h:mm:ss [<small>]A[</small>]"
+);
+}
 
 }
-}
+
 
 
 function updateCity(event) {
@@ -52,6 +67,7 @@ function updateCity(event) {
   <a href="/">All cities</a>
   `;
 }
+
 
 
 updateTime();
